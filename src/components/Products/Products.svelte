bc360a0d5df52f1b5a6b32c5357ac5f198831c57
esteloabell <manitobaaa@gamil.com>
2020-03-02 19:38:21 -0600

    updated with product store setup

---
+++ src/components/Products/Products.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { onMount, onDestroy } from 'svelte';
+  import products from '../../stores/defaultProducts';
+  let localProducts = [];
+  onMount(() => {
+    products.subscribe(value => {
+      localProducts = value;
+    });
+  });
+</script>
+
+<h1>hello from product component</h1>
+{#each localProducts as products}
+  <h2>{products.title}</h2>
+{/each}
