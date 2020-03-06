1b7a07df5e1521bdf15b78681e2dd128a4d84465
esteloabell <manitobaaa@gamil.com>
2020-03-05 19:22:17 -0600

    added transition on sidebar

--- src/components/Navbar/Navbar.svelte
+++ src/components/Navbar/Navbar.svelte
@@ -1,8 +1,12 @@
 <script>
   import SmallNavbar from './SmallNavbar.svelte';
   import BigNavbar from './BigNavbar.svelte';
-  let screenWidth;
+  import globalStore from '../../stores/globalStore';
 
+  let screenWidth;
+  $: if (screenWidth > 992) {
+    globalStore.toggleItem('sidebar', false);
+  }
 </script>
 
 <svelte:window bind:innerWidth={screenWidth} />
--- src/components/Navbar/Sidebar.svelte
+++ src/components/Navbar/Sidebar.svelte
@@ -2,15 +2,16 @@
   import links from '../../constants/links';
   import { link } from 'svelte-routing';
   import globalStore from '../../stores/globalStore';
+  import {fly, fade} from 'svelte/transition';
 </script>
 
-<div class="sidebar-container">
-  <div class="sidebar">
+<div class="sidebar-container" transition:fly={{ x:-1000 }} >
+  <div class="sidebar" transition:fade={{ delay:400 }}>
     <!-- header -->
     <div class="sidebar-header">
       <button class="btn-close" 
       on:click="{()=>{
-        globalStore.toggleItem('sidebar', false)
+        globalStore.toggleItem('sidebar', false);
       }}">
         <i class="fas fa-window-close" />
       </button>
@@ -22,8 +23,8 @@
         {#each links as sideLink}
           <li>
             <a href="{sideLink.url}" use:link 
-            on:click="{() => {
-              globalStore.toggleItem('sidebar', false)
+            on:click="{() => { 
+              globalStore.toggleItem('sidebar', false);
             }}"
             >{sideLink.text}</a>
           </li>
